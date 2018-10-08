b3c0d6293f72175b79e889dbc07097c4c5455f3b
ArthurTemporim <arthurrtl@gmail.com>
2018-10-08 14:12:45 -0300

    Improve the output of the rasa api.
    Signed-off-by: ArthurTemporim <arthurrtl@gmail.com>

--- scripts/api.py
+++ scripts/api.py
@@ -1,23 +1,40 @@
 import requests
 
-#URL = 'http://localhost:5005/conversations/default/respond'
-#PARAMS = {'query':'hi'}
-#request = requests.get(url = URL, params = PARAMS)
-#data = request.json()
-#print(data)
+asks = ['hi', 'good', 'hi', 'bad', 'yes', 'hi', 'bad', 'no']
+answers = []
+answers_data = []
 
-URL = 'http://localhost:5005/conversations/default/tracker'
-request = requests.get(url = URL)
-data = request.json()
-print(len(data))
-print('-'*20)
-print(data)
-print('-'*20)
-#print(data['events'][1]['parse_data']['intent'])
-for e in data['events']:
-    #print(e)
-    if 'parse_data' in e:
-        print(e['parse_data']['intent'])
-        print('-'*50)
-#for i in range(len(data)):
-#    print(data['events'][i]['parse_data']['intent'])
+def sendMessage(message):
+    URL = 'http://localhost:5005/conversations/default/respond'
+    PARAMS = {'query':message}
+    request = requests.get(url = URL, params = PARAMS)
+    data = request.json()
+    answers.append(data)
+
+def get_data():
+    URL = 'http://localhost:5005/conversations/default/tracker'
+    request = requests.get(url = URL)
+    data = request.json()
+    #print(len(data))
+    #print('-'*20)
+    #print(data)
+    #print('-'*20)
+    for e in data['events']:
+        if 'parse_data' in e:
+            #print(e['parse_data']['intent'])
+            #print('-'*50)
+            answers_data.append(e['parse_data']['intent'])
+    print(len(answers))
+    i = 0
+    for i in range(len(answers)):
+        print(i)
+        print('Ask:',asks[i])
+        print('Answer:',answers[i][0]['text'])
+        print(answers_data[i])
+        i+=1
+    #for i in range(len(data)):
+    #    print(data['events'][i]['parse_data']['intent'])
+
+for ask in asks:
+    sendMessage(ask)
+get_data()
